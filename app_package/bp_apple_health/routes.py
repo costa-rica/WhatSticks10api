de64c169bc9ffb75fb93c1a49c2ec652405e35ac
Nick Rodriguez <nrodrig1@gmail.com>
2023-12-02 12:43:05 +0100

    receive_steps save data to database

--- app_package/bp_apple_health/routes.py
+++ app_package/bp_apple_health/routes.py
@@ -39,30 +39,49 @@ logger_bp_apple_health.info(f'- WhatSticks10 API users Bluprints initialized')
 def receive_steps(current_user):
     logger_bp_apple_health.info(f"- accessed  receive_steps endpoint-")
 
-
     try:
         request_json = request.json
     except Exception as e:
         logger_bp_apple_health.info(e)
         return jsonify({"status": "httpBody data recieved not json not parse-able."})
     
-    logger_bp_apple_health.info(f"- ------------------------- -")
     logger_bp_apple_health.info(f"- Count of Apple Health Data: {len(request_json)} -")
-    logger_bp_apple_health.info(f"- ------------------------- -")
-    
     logger_bp_apple_health.info(f"- request_json: {type(request_json)} -")
     timestamp = datetime.now().strftime('%Y%m%d-%H%M')
     json_data_path_and_name = os.path.join(current_app.config.get('APPLE_HEALTH_DIR'),f"AppleHealth-user_id{current_user.id}-{timestamp}.json")
 
     with open(json_data_path_and_name, 'w') as file:
         json.dump(request_json, file, indent=4)
-
-    # response_dict = {}
-
-    # for apple_health_entry in request_json:
-    #     logger_bp_apple_health.info(f"- request_json key: {apple_health_entry} -")
-        # response_dict[key]=value
     
     count_of_entries = len(request_json)
+
+    # for entry in json_data2[0:20]:
+    for entry in request_json:
+            # Check if a record with the same user_id, UUID, and sampleType already exists
+        exists = sess.query(AppleHealhKit).filter_by(
+            user_id=1, 
+            UUID=entry.get('UUID'), 
+            sampleType=entry.get('sampleType')
+        ).first() is not None
+
+        if not exists:
+        
+            new_entry = AppleHealhKit(user_id = 1,
+                sampleType = entry.get('sampleType'),
+                startDate = entry.get('startDate'),
+                endDate = entry.get('endDate'),
+                metadataAppleHealth = entry.get('metadata'),
+                sourceName = entry.get('sourceName'),
+                sourceVersion = entry.get('sourceVersion'),
+                sourceProductType = entry.get('sourceProductType'),
+                device = entry.get('device'),
+                UUID = entry.get('UUID'),
+                quantity = entry.get('quantity'))
+            sess.add(new_entry)
+            sess.commit()
+            print(f"UUID {entry.get('UUID')} Added ****")
+        else:
+            print(f"UUID {entry.get('UUID')} already exists")
+
 
     return jsonify({"Message":"Success! We got the data.", "count_of_entries":count_of_entries })
