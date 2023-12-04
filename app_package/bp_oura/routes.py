9255d0bab5fbb16478969cb04e4aa0df426b8a62
Nick Rodriguez <nrodrig1@gmail.com>
2023-12-04 13:47:26 +0100

    bp_oura removed need for ws_api_password

--- app_package/bp_oura/routes.py
+++ app_package/bp_oura/routes.py
@@ -53,20 +53,20 @@ def add_oura_token(current_user):
         response = jsonify({"error": str(e)})
         return make_response(response, 400)
 
-    if current_app.config.get('WS_API_PASSWORD') == request_json.get('WS_API_PASSWORD'):
+    # if current_app.config.get('WS_API_PASSWORD') == request_json.get('WS_API_PASSWORD'):
 
-        request_data = request.get_json()
-        new_oura_token = request_data.get('oura_token')
-        logger_bp_oura.info(f'new_oura_token: {new_oura_token}')
-        new_token_record = OuraToken(token=new_oura_token, user_id=current_user.id)
-        sess.add(new_token_record)
-        sess.commit()
-        response_dict = {}
-        response_dict["message"] = f"Successfully added token for {current_user.email} !"
-        return jsonify(response_dict)
-    else:
-        response = jsonify({"error": 'Could not verify sender'})
-        return make_response(response, 401)
+    request_data = request.get_json()
+    new_oura_token = request_data.get('oura_token')
+    logger_bp_oura.info(f'new_oura_token: {new_oura_token}')
+    new_token_record = OuraToken(token=new_oura_token, user_id=current_user.id)
+    sess.add(new_token_record)
+    sess.commit()
+    response_dict = {}
+    response_dict["message"] = f"Successfully added token for {current_user.email} !"
+    return jsonify(response_dict)
+    # else:
+    #     response = jsonify({"error": 'Could not verify sender'})
+    #     return make_response(response, 401)
 
 @bp_oura.route('/add_oura_sleep_sessions', methods=['POST'])
 @token_required
@@ -86,42 +86,42 @@ def add_oura_sleep_sessions(current_user):
         response = jsonify({"error": str(e)})
         return make_response(response, 400)
 
-    if current_app.config.get('WS_API_PASSWORD') == request.json.get('WS_API_PASSWORD'):
+    # if current_app.config.get('WS_API_PASSWORD') == request.json.get('WS_API_PASSWORD'):
 
-        # Get current date and time
-        # current_time = datetime.now().strftime("%Y%m%d%H%M%S")
-        current_date = datetime.now().strftime("%Y%m%d")
-        # Construct the file_name_oura_json
-        file_name_oura_json = f"oura_sleep-{current_date}-user_id{current_user.id}.json"
+    # Get current date and time
+    # current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+    current_date = datetime.now().strftime("%Y%m%d")
+    # Construct the file_name_oura_json
+    file_name_oura_json = f"oura_sleep-{current_date}-user_id{current_user.id}.json"
 
-        if os.path.exists(os.path.join(current_app.config.get('DIR_DB_AUX_OURA_SLEEP_RESPONSES'),file_name_oura_json)):
-            
-            logger_bp_oura.info(f"- FOUND file named: {os.path.join(current_app.config.get('DIR_DB_AUX_OURA_SLEEP_RESPONSES'),file_name_oura_json)}")
+    if os.path.exists(os.path.join(current_app.config.get('DIR_DB_AUX_OURA_SLEEP_RESPONSES'),file_name_oura_json)):
+        
+        logger_bp_oura.info(f"- FOUND file named: {os.path.join(current_app.config.get('DIR_DB_AUX_OURA_SLEEP_RESPONSES'),file_name_oura_json)}")
 
-            with open(os.path.join(current_app.config.get('DIR_DB_AUX_OURA_SLEEP_RESPONSES'),file_name_oura_json), 'r') as file:
-                response_oura_sleep = json.load(file)
-            
-            response_dict = add_oura_sleep_to_OuraSleepDescriptions(current_user.id, current_user.oura_token_id[0].token, response_oura_sleep)
-            response_dict['message']= f"Already have Oura download for today  {os.path.join(current_app.config.get('DIR_DB_AUX_OURA_SLEEP_RESPONSES'),file_name_oura_json)} !"
-            return jsonify(response_dict)
-            
-        else:
-            logger_bp_oura.info(f"No file named: {os.path.join(current_app.config.get('DIR_DB_AUX_OURA_SLEEP_RESPONSES'),file_name_oura_json)}")
-            # get user's token
-            OURA_API_TOKEN =  sess.query(OuraToken).filter_by(user_id=current_user.id).first().token
-            # call oura
-            response_oura_sleep = requests.get(current_app.config.get('OURA_API_URL_BASE'), headers={"Authorization": "Bearer " + OURA_API_TOKEN})
-
-            # save response to JSON file in DIR_DB_AUX_OURA_SLEEP_RESPONSES with name: oura_sleep-YYYYMMDDHHSS-user_id.json
-            with open(os.path.join(current_app.config.get('DIR_DB_AUX_OURA_SLEEP_RESPONSES'),file_name_oura_json), 'w') as file:
-                json.dump(response_sleep.json(), file)
-
-            response_dict = add_oura_sleep_to_OuraSleepDescriptions(current_user.id, current_user.oura_token_id[0].token, response_oura_sleep)
-            response_dict['message']= f"Successfully saved oura json data  {os.path.join(current_app.config.get('DIR_DB_AUX_OURA_SLEEP_RESPONSES'),file_name_oura_json)} !"
-            return jsonify(response_dict)
-
+        with open(os.path.join(current_app.config.get('DIR_DB_AUX_OURA_SLEEP_RESPONSES'),file_name_oura_json), 'r') as file:
+            response_oura_sleep = json.load(file)
+        
+        response_dict = add_oura_sleep_to_OuraSleepDescriptions(current_user.id, current_user.oura_token_id[0].token, response_oura_sleep)
+        response_dict['message']= f"Already have Oura download for today  {os.path.join(current_app.config.get('DIR_DB_AUX_OURA_SLEEP_RESPONSES'),file_name_oura_json)} !"
+        return jsonify(response_dict)
+        
     else:
-        response = jsonify({"error": 'Could not verify sender'})
-        return make_response(response, 401)
+        logger_bp_oura.info(f"No file named: {os.path.join(current_app.config.get('DIR_DB_AUX_OURA_SLEEP_RESPONSES'),file_name_oura_json)}")
+        # get user's token
+        OURA_API_TOKEN =  sess.query(OuraToken).filter_by(user_id=current_user.id).first().token
+        # call oura
+        response_oura_sleep = requests.get(current_app.config.get('OURA_API_URL_BASE'), headers={"Authorization": "Bearer " + OURA_API_TOKEN})
+
+        # save response to JSON file in DIR_DB_AUX_OURA_SLEEP_RESPONSES with name: oura_sleep-YYYYMMDDHHSS-user_id.json
+        with open(os.path.join(current_app.config.get('DIR_DB_AUX_OURA_SLEEP_RESPONSES'),file_name_oura_json), 'w') as file:
+            json.dump(response_sleep.json(), file)
+
+        response_dict = add_oura_sleep_to_OuraSleepDescriptions(current_user.id, current_user.oura_token_id[0].token, response_oura_sleep)
+        response_dict['message']= f"Successfully saved oura json data  {os.path.join(current_app.config.get('DIR_DB_AUX_OURA_SLEEP_RESPONSES'),file_name_oura_json)} !"
+        return jsonify(response_dict)
+
+    # else:
+    #     response = jsonify({"error": 'Could not verify sender'})
+    #     return make_response(response, 401)
 
 
