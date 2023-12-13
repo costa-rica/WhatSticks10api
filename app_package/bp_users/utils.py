3c363666a0571e7d6efbea4a9a89f927a45240cf
Nick Rodriguez <nrodrig1@gmail.com>
2023-12-13 18:42:30 +0100

    confirmation email - minor mod

--- app_package/bp_users/utils.py
+++ app_package/bp_users/utils.py
@@ -7,7 +7,7 @@ import os
 import shutil
 import logging
 from logging.handlers import RotatingFileHandler
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+# from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 
 #Setting up Logger
