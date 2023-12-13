06ab75b1aa66af20d02d9d15ed24c30411804ae6
Nick Rodriguez <nrodrig1@gmail.com>
2023-12-13 06:46:55 +0100

    minor mod bp_apple_health

--- app_package/bp_apple_health/routes.py
+++ app_package/bp_apple_health/routes.py
@@ -97,7 +97,7 @@ def receive_apple_health_data(current_user):
         response_dict = add_apple_health_to_database(current_user.id, apple_health_data_request_json_file_name)
     else:
         # send email
-        path_sub = os.path.join(current_app.config.get('APPLE_SERVICE_ROOT'), 'add_apple_health_to_database.py')
+        path_sub = os.path.join(current_app.config.get('APPLE_SERVICE_ROOT'), 'apple_health_service.py')
         subprocess.Popen(['python', path_sub,str(current_user.id),apple_health_data_request_json_file_name ])
 
     return jsonify(response_dict)
