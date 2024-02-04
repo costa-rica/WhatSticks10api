1e9ed834cb031e3949fd2318852edcbf8770757b
Nick Rodriguez <nrodrig1@gmail.com>
2024-02-03 17:17:08 -0700

    added location_permission to login dictionary

--- app_package/bp_users/routes.py
+++ app_package/bp_users/routes.py
@@ -81,6 +81,7 @@ def login():
             user_object_for_swift_app['password'] = "test"
             user_object_for_swift_app['token'] = serializer.dumps({'user_id': user.id})
             user_object_for_swift_app['timezone'] = user.timezone
+            user_object_for_swift_app['location_permission'] = user.location_permission
             oura_token_obj = sess.query(OuraToken).filter_by(user_id=user.id).first()
             if oura_token_obj and oura_token_obj.token is not None:
                 user_object_for_swift_app['oura_token'] = oura_token_obj.token
